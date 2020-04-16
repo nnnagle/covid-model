e9aab851989ed92514684eac6600c0c6e546f612
Nicholas Nagle <nnagle@utk.edu>
2020-04-16 11:34:56 -0400

    - cosmetic formatting of 02-run_diagnostics

--- analysis-reformat/02-run_diagnostics.R
+++ analysis-reformat/02-run_diagnostics.R
@@ -1,6 +1,6 @@
 #ROOT <- '/home/nnagle/Dropbox/students/Piburn/covid-model'
-source('analysis/00-PARAMS.R')
-source('analysis/00-functions.R')
+source('analysis-reformat/00-PARAMS.R')
+source('analysis-reformat/00-functions.R')
 
 # This uses the future library and furrr package for multithreading.
 # I use availableCores()-4 on my own machine
@@ -9,14 +9,15 @@ source('analysis/00-functions.R')
 # The output is a small diagnostic.Rdata file in DATA_DIR
 
 library(tidyverse)
-library(vroom)
+#library(vroom)
 library(parallel)
 library(furrr)
 
 FIPS='01 02 04 05 06 08 09 10 12 13 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50 51 53 54 55 56'
 
 
-cl <- makeClusterPSOCK(availableCores() - 70)
+#cl <- makeClusterPSOCK(availableCores() - 70)
+cl <- makeClusterPSOCK(NNODES)
 plan(cluster, workers = cl)
 
 
@@ -25,18 +26,18 @@ plan(cluster, workers = cl)
   
 # Evaluate Rhat for the parameter bo_raw
 # using all chains
-diagnostic_df <- tibble(State = strsplit(FIPS, split=' ')[[1]]) %>%
-  mutate(files = future_map(.x=State, ~get_sample_paths(.x, DATA_DIR)), .progress = TRUE) %>%
-  mutate(diag = future_map(.x=files, ~read_and_summarize(.x, WARMUP, 
-                                                  par_select=c(starts_with('b0_raw')))), .progress = TRUE)
-# Repeat, leaving out chain k
-for(k in 1:NCHAINS){
-  varname <- paste0('diag', k)
-  diagnostic_df <- diagnostic_df %>%
-    mutate({{varname}} := future_map(.x=files, ~read_and_summarize(.x, WARMUP,
-                                                            par_select=c(starts_with('b0_raw')),
-                                                            k=k)))
-}
+diagnostic_df <- 
+  tibble(
+    State = strsplit(FIPS, split=' ')[[1]]) %>%
+  mutate(
+    files = future_map(.x=State, 
+                       ~get_sample_paths(.x, DATA_DIR))) %>%
+  mutate(
+    diag = future_map(.x=files, 
+                      ~read_and_diagnose(.x, 
+                                         warmup=WARMUP, 
+                                         par_select=c(starts_with('tau'),
+                                                      starts_with('log_lambda')))))
 
 ###################################################################
 # Determine which chains to use here....
