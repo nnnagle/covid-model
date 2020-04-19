aa8080e10f5729e5afef55d97b6d7d79a6c06767
Nicholas Nagle <nnagle@utk.edu>
2020-04-19 17:50:59 -0400

    cosmetic changes to 02-run_diagnostics

--- analysis-reformat/02-run_diagnostics.R
+++ analysis-reformat/02-run_diagnostics.R
@@ -11,12 +11,13 @@ source('analysis-reformat/00-functions.R')
 library(tidyverse)
 #library(vroom)
 library(parallel)
-library(tidyr)
+#library(tidyr)
 library(furrr)
 #library(rstan)
 
 FIPS='01 02 04 05 06 08 09 10 12 13 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50 51 53 54 55 56'
 
+#FIPS = paste(basename(list.dirs(DATA_DIR))[-1], collapse=' ' )
 
 #cl <- makeClusterPSOCK(availableCores() - 70)
 cl <- makeClusterPSOCK(NNODES)
@@ -28,7 +29,6 @@ plan(cluster, workers = cl)
   
 # Evaluate Rhat for the parameter bo_raw
 # using all chains
-#<<<<<<< HEAD
 diagnostic_df <- 
   tibble(
     State = strsplit(FIPS, split=' ')[[1]]) %>%
