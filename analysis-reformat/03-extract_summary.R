275aac8b45829ca094c9f0795231c6d0079b561f
nnnagle <nnnagle@utk.edu>
2020-07-14 16:11:14 -0400

    adding state summary to 03-extract_summary

--- analysis-reformat/03-extract_summary.R
+++ analysis-reformat/03-extract_summary.R
@@ -8,7 +8,7 @@
 
 
 #source('analysis-reformat/00-PARAMS.R')
-source('/data/covid/tmp/2020-06-01/00-PARAMS.R')
+source('/data/covid/tmp/2020-07-10/00-PARAMS.R')
 
 
 source('analysis-reformat/00-functions.R')
@@ -145,7 +145,75 @@ data_out <-
   select(-mygeoid, -mystate, -i, -t)
 
 
-# Save the data.
+# Save the county data.
 save(data_out, file=file.path(RESULTS_DIR, paste0('results_', DATE,'.RData')))
 
 
+#################################################################################
+# create state summary
+summary_mu <- 
+  summary_df %>%
+  mutate(
+    mu_summary = future_map2(.x=State, .y=good_files, function(x,y){
+      if(length(y)==0){
+        return(tibble(mu_mean = NA,
+                      mu_q05 = NA,
+                      mu_q15 = NA,
+                      mu_q25 = NA,
+                      mu_q50 = NA,
+                      mu_q75 = NA,
+                      mu_q85 = NA,
+                      mu_q95 = NA))
+      }
+      samples <- read_stan_draws(y, par_select=c(starts_with("log_lambda")), warmup = WARMUP)
+      summary <- samples %>% 
+        separate(.variable, into=c('.variable','i','t'), sep = '\\.') %>%
+        mutate(i=as.integer(i)) %>%
+        mutate(State=x) %>%
+        left_join(
+          county_df %>% select(mystate, state_name, i, pop),   
+          by = c('State'='mystate', 'i')) %>%
+        group_by(.draw, t, state_name) %>%
+        summarize(.value = sum(exp(.value)*pop, na.rm=TRUE)) %>%
+        drop_na() %>%
+        ungroup() %>% 
+        group_by(t, state_name) %>%
+        summarize(
+                  mu_q05 = (quantile(.value, .05)),
+                  mu_q15 = (quantile(.value, .15)),
+                  mu_q25 = (quantile(.value, .25)),
+                  mu_q50 = (quantile(.value, .5)),
+                  mu_q75 = (quantile(.value, .75)),
+                  mu_q85 = (quantile(.value, .85)),
+                  mu_q95 = (quantile(.value, .95))) %>%
+        ungroup()
+      summary <- summary %>%
+        mutate(
+          t=as.integer(t)) 
+      return(summary)
+    }
+    )
+  ) %>%
+  select(
+    State, 
+    mu_summary) %>%
+  unnest(
+    mu_summary)
+
+# Fix the FIPS code for DC
+summary_mu %>%
+  mutate(State=if_else(state_name=='District of Columbia','11', State ))
+
+state_out <- 
+  summary_mu %>%
+  left_join(county_df %>%
+              select(state_name, pop) %>% 
+              group_by(state_name) %>%
+              summarize(pop = sum(pop)),
+            by=c('state_name')) %>%
+  left_join(date_df, by='t') %>%
+  arrange(State, t) %>%
+  select(-t)
+
+# Save the state data.
+save(state_out, file=file.path(RESULTS_DIR, paste0('results_state_', DATE,'.RData')))
